6d1ef377514ec509cc361978648986897969ebcb
Alexandr Tatarnikov <colmoon@ya.ru>
2020-08-07 16:16:42 +0300

    add router user

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const user = require('./routes/users');
 
 const app = express();
 
@@ -9,6 +10,9 @@ mongoose.connect('mongodb://localhost:27017/moestrodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
+
+app.use('/users', user);
 
 app.listen(PORT);
