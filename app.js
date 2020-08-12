407c037973215cf45d5b14df86e1a989a1727310
Alexandr <colmoon@yandex.ru>
2020-08-12 20:41:36 +0300

    add useUnifiedTopology: true, body-parser.json(), tru;catch

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const user = require('./routes/users');
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
+const cards = require('./routes/cards');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/moestrodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+async function start() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/moestrodb', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = { _id: '5f2fc839e47a9c32d89d96d8' };
+  next();
 });
 
-app.use('/users', user);
+app.use('/users', users);
+app.use('/cards', cards);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
-app.listen(PORT);
+start();
